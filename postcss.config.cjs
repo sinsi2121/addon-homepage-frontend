9513fcb243930d64ee24a6a2dee3edc62d65b618
sinsi2121 <yousheyoun913@gmail.com>
2026-03-28 12:39:51 +0900

    postcss.config.cjsを修正: 空の設定に変更

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,3 @@
-// postcss.config.cjs
-module.exports = {
-plugins: {
-tailwindcss: {},
-autoprefixer: {},
-},
-};
-
-Tailwind CSSやAutoprefixerを使わない場合は、こちら：
-
-// postcss.config.cjs
 module.exports = {
 plugins: [],
 };
